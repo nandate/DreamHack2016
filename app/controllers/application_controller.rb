e0629c989fbe972be4b0225957a1ca43eeb584d4
nandate <o238049t@mail.kyutech.jp>
2016-09-06 12:30:42 +0900

    ログインの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :user_permitted_parameters,if: :devise_controller?
+
+  private
+  def user_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up){|u| u.permit(:name,:email,:age,:address,:password,:password_confirmation)}
+  end
+
 end
