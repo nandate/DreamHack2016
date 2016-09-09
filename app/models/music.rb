40bd73d024465093da4574c623b8f7bce66dacfa
nandate <o238049t@mail.kyutech.jp>
2016-09-09 18:01:13 +0900

    いいねが多い順に並べ替えた

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,6 +1,5 @@
 class Music < ActiveRecord::Base
   belongs_to :artist
-  default_scope->{order(created_at: :desc)}
   mount_uploader :image,ThumbnailUploader
   has_many :likes,dependent: :destroy
   def like_user(user_id)
